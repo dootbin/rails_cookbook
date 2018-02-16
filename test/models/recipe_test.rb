6fa087a17db3671b7e446ac43518c95d47fa0077
Benjamin Miles <dootbin@dootbin>
2018-02-16 15:21:55 -0600

    chapter 6 work completed

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -26,4 +26,10 @@ class RecipeTest < ActiveSupport::TestCase
     assert !recipe.save
     assert recipe.errors[:instructions].include?("can't be blank")
   end 
+
+  test "should only have one recipe with the same name" do
+    @recipe = Recipe.new(:title =>"Toast")
+    @recipe.valid?
+    assert @recipe.errors[:title]
+  end
 end
