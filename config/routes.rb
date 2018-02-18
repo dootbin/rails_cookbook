3e0976f4e6974b55d4c5f933719a82204226a26f
Benjamin Miles <dootbin@dootbin>
2018-02-18 14:45:49 -0600

    added admin

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "/about", controller: 'home', action: 'about', as: 'about'
   root controller: 'home', action: 'index'
   get "home/about"
-  
+  get "/admin", controller: 'admin', action: 'index'  
 # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
