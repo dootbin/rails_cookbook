607fb95bab6bc5b0e8fd7d5410e99286162d9b89
Benjamin Miles <dootbin@gmail.com>
2018-03-11 05:54:42 -0500

    added find_all_by_query

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,13 @@ class Recipe < ActiveRecord::Base
   belongs_to :category
   validates :title, uniqueness: true
   validates_presence_of :title, :ingredients, :instructions
+
+  def self.find_all_by_query(query)
+    query = query.downcase
+    query = "%#{query}%"
+
+    Recipe.where(['lower(title) like ? ', query])
+  end
+
 end
+
